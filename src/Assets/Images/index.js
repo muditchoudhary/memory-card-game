ef4dd7df3f1b9d8f74d6ffe39166c34062ec6b52
Mudit Choudhary <cmudit60@gmail.com>
2023-03-24 13:59:28 +0530

    Added Images for the game.
    Add a index.js file which will import all the images first and
    then export all of them into a object. This way I prevent the
    clutter into the App.js file  due to so many imports.

---
+++ src/Assets/Images/index.js
@@ -0,0 +1,111 @@
+import Apple from "./Apple.jpg";
+import Apricot from "./Apricot.jpg";
+import Avacoda from "./Avacoda.jpg";
+import Banana from "./Banana.jpg";
+import Blueberries from "./Blueberries.jpg";
+import Blackberry from "./Blackberry.jpg";
+import Black_current from "./Black_current.jpg";
+import Bell_fruit from "./Bell_fruit.jpg";
+import Custard_apple from "./Custard_apple.jpg";
+import Coconut from "./Coconut.jpg";
+import Cashew from "./Cashew.jpg";
+import Cherry from "./Cherry.jpg";
+import Cucumberfruit from "./Cucumberfruit.jpg";
+import Currant from "./Currant.jpg";
+import Cranberry from "./Cranberry.jpg";
+import Date from "./Date.jpg";
+import Dragon from "./Dragon.jpg";
+import Durian from "./Durian.jpg";
+import Elderberry from "./Elderberry.jpg";
+import Fig from "./Fig.jpg";
+import Guava from "./Guava.jpg";
+import Grapes from "./Grapes.jpg";
+import Goosebery from "./Goosebery.jpg";
+import Jackfruit from "./Jackfruit.jpg";
+import Jambool from "./Jambool.jpg";
+import Jujube from "./Jujube.jpg";
+import Kiwi from "./Kiwi.jpg";
+import Kokum from "./Kokum.jpg";
+import Lemon from "./Lemon.jpg";
+import Lychees from "./Lychees.jpg";
+import Longan from "./Longan.jpg";
+import Mango from "./Mango.jpg";
+import Mulberry from "./Mulberry.jpg";
+import Orange from "./Orange.jpg";
+import Pineapple from "./Pineapple.jpg";
+import Passon from "./Passon.jpg";
+import Peach from "./Peach.jpg";
+import Pomegranate from "./Pomegranate.jpg";
+import Palm_fruit from "./Palm_fruit.jpg";
+import Pear from "./Pear.jpg";
+import Plum from "./Plum.jpg";
+import Papaya from "./Papaya.jpg";
+import Pithecellobium_dulce from "./Pithecellobium_dulce.jpg";
+import Raspberry from "./Raspberry.jpg";
+import Rambutan from "./Rambutan.jpg";
+import Strawberry from "./Strawberry.jpg";
+import Sapota from "./Sapota.jpg";
+import Star_fruit from "./Star_fruit.jpg";
+import Sweet_lime from "./Sweet_lime.jpg";
+import Sugercane from "./Sugercane.jpg";
+import Tamarind from "./Tamarind.jpg";
+import Watermelon from "./Watermelon.jpg";
+import Wood_apple from "./Wood_apple.jpg";
+
+const fruitImages = {
+	Apple: Apple,
+	Apricot: Apricot,
+	Avacoda: Avacoda,
+	Banana: Banana,
+	Blueberries: Blueberries,
+	Blackberry: Blackberry,
+	Black_current: Black_current,
+	Bell_fruit: Bell_fruit,
+	Custard_apple: Custard_apple,
+	Coconut: Coconut,
+	Cashew: Cashew,
+	Cherry: Cherry,
+	Cucumberfruit: Cucumberfruit,
+	Currant: Currant,
+	Cranberry: Cranberry,
+	Date: Date,
+	Dragon: Dragon,
+	Durian: Durian,
+	Elderberry: Elderberry,
+	Fig: Fig,
+	Guava: Guava,
+	Grapes: Grapes,
+	Goosebery: Goosebery,
+	Jackfruit: Jackfruit,
+	Jambool: Jambool,
+	Jujube: Jujube,
+	Kiwi: Kiwi,
+	Kokum: Kokum,
+	Lemon: Lemon,
+	Lychees: Lychees,
+	Longan: Longan,
+	Mango: Mango,
+	Mulberry: Mulberry,
+	Orange: Orange,
+	Pineapple: Pineapple,
+	Passon: Passon,
+	Peach: Peach,
+	Pomegranate: Pomegranate,
+	Palm_fruit: Palm_fruit,
+	Pear: Pear,
+	Plum: Plum,
+	Papaya: Papaya,
+	Pithecellobium_dulce: Pithecellobium_dulce,
+	Raspberry: Raspberry,
+	Rambutan: Rambutan,
+	Strawberry: Strawberry,
+	Sapota: Sapota,
+	Star_fruit: Star_fruit,
+	Sweet_lime: Sweet_lime,
+	Sugercane: Sugercane,
+	Tamarind: Tamarind,
+	Watermelon: Watermelon,
+	Wood_apple: Wood_apple,
+};
+
+export default fruitImages;
